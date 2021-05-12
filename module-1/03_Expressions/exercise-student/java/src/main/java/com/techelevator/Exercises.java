4fc06bd285607b66299315460c31b6efa1aa24b8
Jared Crist <cristjl1@gmail.com>
2021-05-12 16:31:08 -0400

    Question 15

--- module-1/03_Expressions/exercise-student/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/03_Expressions/exercise-student/java/src/main/java/com/techelevator/Exercises.java
@@ -301,6 +301,19 @@ public class Exercises {
 	 max1020(11, 9) → 11
 	 */
 	public int max1020(int a, int b) {
+
+		if ((a >= 10 && a <=20) && (b >=10 && b <=20)) {
+			return Math.max(a,b);
+		}
+
+		if ((a >= 10 && a <=20) && (b <10 || b > 20)) {
+			return a;
+		}
+
+		if ((b >= 10 && b <=20) && (a <10 || a > 20)) {
+			return b;
+		}
+
 		return 0;
 	}
 
