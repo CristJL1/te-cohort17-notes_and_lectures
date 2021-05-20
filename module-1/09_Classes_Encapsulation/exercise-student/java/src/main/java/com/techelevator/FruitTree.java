bb7a6b38f59f5ccbe87d768f3a99f4ae84706a35
Jared Crist <cristjl1@gmail.com>
2021-05-20 17:44:02 -0400

    2nd one done

--- module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/FruitTree.java
+++ module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/FruitTree.java
@@ -2,4 +2,54 @@ package com.techelevator;
 
 public class FruitTree {
 
+        private String typeOfFruit;
+
+        private  int piecesOfFruitLeft;
+
+
+
+
+    public String getTypeOfFruit() {
+        return typeOfFruit;
+    }
+
+    public int getPiecesOfFruitLeft() {
+        return piecesOfFruitLeft;
+    }
+
+    public boolean pickFruit(int numberOfPiecesToRemove) {
+
+        if (piecesOfFruitLeft >= numberOfPiecesToRemove) {
+            piecesOfFruitLeft = piecesOfFruitLeft - numberOfPiecesToRemove;
+            return true;
+        }
+        else {
+            return false;
+        }
+
+
+
+    }
+
+
+
+    public FruitTree () {
+
+        typeOfFruit = null;
+        piecesOfFruitLeft = 0;
+
+    }
+
+    public FruitTree (String fruitType, int startingPiecesOfFruit) {
+
+        typeOfFruit = fruitType;
+        piecesOfFruitLeft = startingPiecesOfFruit;
+
+
+    }
+
+
+
+
+
 }
