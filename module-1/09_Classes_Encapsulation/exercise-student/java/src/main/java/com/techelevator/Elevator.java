0ea23c8d0b757a6c232299515d9cc5d662ddf252
Jared Crist <cristjl1@gmail.com>
2021-05-20 20:47:59 -0400

    just airplane left

--- module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,80 @@ package com.techelevator;
 
 public class Elevator {
 
-}
+    private int currentFloor;
+
+    private int numberOfFloors;
+
+    private boolean doorOpen;
+
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public boolean isDoorOpen() {
+        return doorOpen;
+    }
+
+    public Elevator () {
+
+        numberOfFloors = 10;
+        currentFloor = 1;
+        doorOpen = true;
+
+    }
+
+
+
+    public Elevator (int numberOfLevels) {
+
+        numberOfFloors = numberOfLevels;
+        currentFloor = 1;
+        doorOpen = false;
+
+    }
+
+    public void openDoor() {
+
+        doorOpen = true;
+
+    }
+
+    public void closeDoor() {
+
+        doorOpen = false;
+    }
+
+    public void goUp (int desiredFloor) {
+
+        if (!doorOpen && desiredFloor <= numberOfFloors && desiredFloor > currentFloor) {
+
+            while (currentFloor < desiredFloor) {
+                currentFloor = currentFloor +1;
+            }
+            }
+
+        }
+
+
+    public void goDown (int desiredFloor) {
+
+        if (!doorOpen && desiredFloor >= 1 && desiredFloor < currentFloor) {
+            while (currentFloor > desiredFloor) {
+                currentFloor = currentFloor -1;
+            }
+            }
+        }
+
+    }
+
+
+
+
+
+
+
