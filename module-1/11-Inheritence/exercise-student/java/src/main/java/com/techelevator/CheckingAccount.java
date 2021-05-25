6478cbb7a111434242021050058745175d94e717
Jared Crist <cristjl1@gmail.com>
2021-05-24 21:53:38 -0400

    CheckingAccount Class Finished, All Finished

--- module-1/11-Inheritence/exercise-student/java/src/main/java/com/techelevator/CheckingAccount.java
+++ module-1/11-Inheritence/exercise-student/java/src/main/java/com/techelevator/CheckingAccount.java
@@ -1,22 +1,27 @@
 package com.techelevator;
 
-public class CheckingAccount {
+public class CheckingAccount extends BankAccount {
 
+    private static final int OVERDRAFT_FEE = 10;
 
+    public CheckingAccount (String accountHolderName, String accountNumber) {
+        super(accountHolderName, accountNumber);
+    }
 
+    public CheckingAccount (String accountHolderName, String accountNumber, int balance) {
+        super(accountHolderName, accountNumber, balance);
+    }
 
+    @Override
+    public int withdraw(int amountToWithdraw) {
 
-
-
-
-
-
-    // subclass constructors must run superclass constructors
-
-    // do 2nd
-
-
-
-
-
+        if (super.getBalance() < 0 && super.getBalance() > -100) {
+            amountToWithdraw = amountToWithdraw + OVERDRAFT_FEE;
+            return super.withdraw(amountToWithdraw);
+        }
+        else if (super.getBalance() - amountToWithdraw <= -100) {
+            return super.getBalance();
+        }
+        return super.withdraw(amountToWithdraw);
+    }
 }
