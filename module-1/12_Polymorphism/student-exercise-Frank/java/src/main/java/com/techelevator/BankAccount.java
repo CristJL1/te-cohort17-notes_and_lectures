a777e2fddecf5b78ac18b7c687ece174ca628bed
Jared Crist <cristjl1@gmail.com>
2021-05-25 18:15:06 -0400

    Working on VIP Status

--- module-1/12_Polymorphism/student-exercise-Frank/java/src/main/java/com/techelevator/BankAccount.java
+++ module-1/12_Polymorphism/student-exercise-Frank/java/src/main/java/com/techelevator/BankAccount.java
@@ -54,4 +54,5 @@ public class BankAccount implements Accountable{
         return getBalance();
     }
 
+
 }
--- module-1/12_Polymorphism/student-exercise-Frank/java/src/main/java/com/techelevator/BankCustomer.java
+++ module-1/12_Polymorphism/student-exercise-Frank/java/src/main/java/com/techelevator/BankCustomer.java
@@ -8,6 +8,53 @@ import java.util.List;
  */
 public class BankCustomer {
 
+    private String name;
+
+    private String address;
+
+    private String phoneNumber;
+
     private List<Accountable> accounts = new ArrayList<>();   // List of Accountable objects
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Accountable[] getAccounts() {
+        return accounts.toArray(new Accountable[accounts.size()]);
+    }
+
+    public void addAccount(Accountable newAccount) {
+                accounts.add(newAccount);
+    }
+
+    public boolean isVip () {
+
+       // need to figure out how to call the balances of CheckingAccount, SavingsAccount, and CreditCardAccount
+       // need to figure out why transfer to SavingsAccount isn't working
+
+        return false;
+    }
+
+
 }
