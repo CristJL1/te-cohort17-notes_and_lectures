b10a7a9d9e9fba9ede543267c5930d3a02db4fac
Jared Crist <cristjl1@gmail.com>
2021-05-27 18:15:41 -0400

    Less20Test partial

---
+++ module-1/14_Unit_Testing/exercise-student/java/src/test/java/com/techelevator/Less20Test.java
@@ -0,0 +1,59 @@
+package com.techelevator;
+
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import static org.junit.Assert.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class Less20Test {
+
+    Less20 testCode = new Less20();
+
+    @Test
+    public void n_is_1_less_than_multiple_of_20 () {
+
+        testCode.isLessThanMultipleOf20(19);
+
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void n_is_2_less_than_multiple_of_20 () {
+
+        testCode.isLessThanMultipleOf20(18);
+
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void n_is_not_divisible_by_20 () {
+
+        testCode.isLessThanMultipleOf20(20);
+
+            assertFalse(false);  //come back to this
+
+    }
+
+
+
+    /*
+        Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38
+        and 39 return true, but 40 returns false.
+        (Hint: Think "mod".)
+        less20(18) → true
+        less20(19) → true
+        less20(20) → false
+
+    public boolean isLessThanMultipleOf20(int n) {
+        boolean oneLessThanMultipleOf20 = n % 20 == 19;
+        boolean twoLessThanMultipleOf20 = n % 20 == 18;
+
+        return oneLessThanMultipleOf20 || twoLessThanMultipleOf20;
+    }*/
+
+}
