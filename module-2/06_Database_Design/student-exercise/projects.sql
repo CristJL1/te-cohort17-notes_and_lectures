2d173bd6409f2c40492150ef4b4c6a6b85f8772f
Jared Crist <cristjl1@gmail.com>
2021-06-14 21:23:01 -0400

    Updated Database

---
+++ module-2/06_Database_Design/student-exercise/projects.sql
@@ -0,0 +1,111 @@
+Begin transaction;
+
+drop table if exists employee cascade;
+drop table if exists department cascade;
+drop table if exists project cascade;
+drop table if exists employee_department cascade;
+drop table if exists employee_project cascade;
+
+create table employee
+(employee_id serial not null,
+job_title varchar (30) not null,
+last_name varchar (25) not null,
+first_name varchar (25) not null,
+gender char (1) not null,
+date_of_birth date not null,
+date_of_hire date not null,
+department varchar (30) not null,
+constraint pk_employee_employee_id 
+primary key (employee_id));
+
+create table project
+(project_id serial not null,
+project_name varchar (50) not null,
+start_date date not null,
+number_of_employees int not null,
+constraint pk_project_project_id 
+primary key (project_id));
+
+create table department
+(department_id serial not null,
+department_name varchar (50) not null,
+number_of_employees int not null,
+constraint pk_department_department_id 
+primary key (department_id));
+
+create table employee_department
+(employee_id integer not null,
+department_id integer not null,
+constraint pk_employee_department_employee_id_department_id 
+primary key (employee_id, department_id));
+
+create table employee_project
+(employee_id integer not null,
+project_id integer not null,
+constraint pk_employee_project_employee_id_project_id 
+primary key (employee_id, project_id));
+
+alter table employee_department
+add foreign key (employee_id) 
+references employee (employee_id);
+
+alter table employee_department
+add foreign key (department_id) 
+references department (department_id);
+
+alter table employee_project
+add foreign key (employee_id) 
+references employee (employee_id);
+
+alter table employee_project
+add foreign key (project_id) 
+references project (project_id);
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Vandelay', 'Art', 'M', '09/23/1959', '05/30/1990', 'Sales', 'Importer/Exporter');
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Costanza', 'George', 'M', '12/23/1953', '05/15/1999', 'Design', 'Architect');
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Benes', 'Elaine', 'F', '08/11/1968', '03/26/2003', 'Design', 'Lead Designer');
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Seinfeld', 'Jerry', 'M', '02/20/1970', '05/15/2009', 'Sales', 'Latex Salesman');
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Kramer', 'Cosmo', 'M', '10/23/1990', '07/19/2015', 'HR', 'Benefits Specialist');
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Costanza', 'Estelle', 'F', '11/08/1987', '06/04/2010', 'HR', 'Manager');
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Costanza', 'Frank', 'M', '04/18/1975', '04/30/1995', 'Design', 'Manager');
+
+insert into employee (last_name, first_name, gender, date_of_birth, date_of_hire, department, job_title)
+values ('Ross', 'Susan', 'F', '08/07/1991', '10/31/2013', 'Sales', 'Manager');
+
+
+insert into department (department_name, number_of_employees)
+values ('Sales', (select count (department) from employee where department = 'Sales'));
+
+insert into department (department_name, number_of_employees)
+values ('HR', (select count (department) from employee where department = 'HR'));
+
+insert into department (department_name, number_of_employees)
+values ('Design', (select count (department) from employee where department = 'Design'));
+
+insert into project (project_name, start_date, number_of_employees)
+values ('Architecture Project', '10/01/2020', (select count (department) from employee where department = 'Design'));
+
+insert into project (project_name, start_date, number_of_employees)
+values ('Latex Sales Project', '04/01/2021', (select count (department) from employee where department = 'Sales'));
+
+insert into project (project_name, start_date, number_of_employees)
+values ('Human Fund Project', '07/01/2020', (select count (department) from employee where department = 'HR'));
+
+insert into project (project_name, start_date, number_of_employees)
+values ('Manager Project', '01/01/2021', (select count (job_title) from employee where job_title = 'Manager'));
+
+
+rollback;
--- module-2/06_Database_Design/student-lecture/lecture.sql
+++ module-2/06_Database_Design/student-lecture/lecture.sql
@@ -14,7 +14,7 @@
 --  CREATE - define a table to the database manager
 --
 --       CREATE TABLE table-name
---       (column-name     datßa-type    NOT NULL,
+--       (column-name     data-type    NOT NULL,
 --        column-name     data-type,
 --        column-name     data-type    DEFAULT   default-value,
 --        column-name     data-type    UNIQUE,
@@ -64,4 +64,68 @@
 --      ALTER TABLE IF EXISTS table-name RENAME TO new-table-name  - Successful if table exists or not
 --
 ---------------------------------------------------------------------------------------------------------------
+Begin transaction;
 
+-- drop any existing copies of the table we're creating
+
+drop table if exists artist cascade; -- will work even if the table doesn't exist
+drop table if exists painting cascade; -- and if it has dependents 
+drop table if exists artist_painting cascade;
+
+create table artist -- table names are usually singular
+(artistId   serial NOT NULL, -- unique identifier for an artist; serial means postgres will generate a unique integer value
+artistName character varying(50), -- name of the artist; allow for variable size names
+                                        -- a maximum size may be specified, if no max size is specified then 32767 is assumed
+                                        -- it is more efficient if you set a max size rather than let it assume 32767
+                                        
+--         constraint name    contraint type   constraint definition                                  
+constraint pk_arist_artistid    Primary Key         (artistId)); -- add a primary key to the table using the PRIMARY_KEY constraint
+-- constraint name usually formatted: abbreviation-of-constraint-table-name-column(s)
+-- tell postgres that artistId is the primary key
+
+
+create table painting
+(paintingId serial not null,            -- database generated unique integer
+paintingTitle character varying (100) not null, -- allow up to a 100 character title
+-- decimal (total-#-digits, #-decimal-places)
+purchasePrice decimal (12,2) not null, -- maximum purchase allowed: +/- 9,999,999,999.99 (12 digits with 2 decimal places)  
+constraint pk_painting_paintingid primary key (paintingid));
+
+create table artist_painting
+(artistId integer not null, -- these will match columns in another table because they are foreign keys
+paintingId integer not null, -- did not use serial because we don't want a new unique value
+constraint pk_artist_painting_artistid_paintingid primary key (artistid, paintingid));
+
+--
+-- use the ALTER statement to add a FOREIGN KEY constraints to the artist_painting table
+-- since it is a dependent of both the artist and painting tables
+-- need one ALTER for each FOREIGN KEY
+--                               
+--               dependent               column-in-dependent        parent(primary-key-of-parent)
+ alter table artist_painting add foreign key (artistid) references artist (artistid);  
+ 
+ alter table artist_painting add foreign key (paintingid) references painting (paintingid);
+ 
+ -- typically add the foreign keys after all the tables are created so the order of create tables doesn't matter
+ -- parent tables must be created before dependents if you don't
+ 
+ insert into artist (artistname)
+ values ('Batman'); -- we only need to specify the non-database generated or not null columns - artistid will be generated
+ 
+ insert into painting (paintingtitle, purchaseprice)
+ values ('The Starry Dark Knight', 12000000);    
+ 
+ insert into painting (paintingtitle, purchaseprice)
+ values ('Gotham', 5000000);     
+ 
+ -- let's add a row to artist_painting for each of our paintings by Batman  
+ -- remember, the databse generated the artistid and paintingid, so we don't know what they are
+ 
+ insert into artist_painting (artistid, paintingid)
+ values ((select artistid from artist where artistname = 'Batman'),(select paintingid from painting where paintingtitle = 'Gotham'));
+ 
+  insert into artist_painting (artistid, paintingid)
+ values ((select artistid from artist where artistname = 'Batman'),(select paintingid from painting where paintingtitle = 'The Starry Dark Knight'));
+                      
+
+commit;
