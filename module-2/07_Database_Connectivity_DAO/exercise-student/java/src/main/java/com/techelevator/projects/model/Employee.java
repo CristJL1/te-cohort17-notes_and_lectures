c14555c13b393291d1eec22735847299e6dd8e6f
Jared Crist <cristjl1@gmail.com>
2021-06-15 18:49:26 -0400

    Employee Class

--- module-2/07_Database_Connectivity_DAO/exercise-student/java/src/main/java/com/techelevator/projects/model/Employee.java
+++ module-2/07_Database_Connectivity_DAO/exercise-student/java/src/main/java/com/techelevator/projects/model/Employee.java
@@ -1,7 +1,85 @@
 package com.techelevator.projects.model;
 
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class Employee {
 	// TODO: Place code to describe the columns in the table row here
+
+    private Long employee_id;
+
+    private int department_id;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String gender;
+
+    private Date birth_date;
+
+    private Date hire_date;
+
+    public Long getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(Long employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public Date getHire_date() {
+        return hire_date;
+    }
+
+    public void setHire_date(Date hire_date) {
+        this.hire_date = hire_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee - " +
+                last_name + ", " + first_name;
+    }
 }
