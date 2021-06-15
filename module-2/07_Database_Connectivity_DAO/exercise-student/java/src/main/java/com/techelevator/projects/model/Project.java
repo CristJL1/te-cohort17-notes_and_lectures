b1f0bfe8ebaf2198d8b4b32ee03926b71042eab7
Jared Crist <cristjl1@gmail.com>
2021-06-15 18:59:14 -0400

    Project Class

--- module-2/07_Database_Connectivity_DAO/exercise-student/java/src/main/java/com/techelevator/projects/model/Project.java
+++ module-2/07_Database_Connectivity_DAO/exercise-student/java/src/main/java/com/techelevator/projects/model/Project.java
@@ -1,7 +1,53 @@
 package com.techelevator.projects.model;
 
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class Project {
 	// TODO: Place code to describe the columns in the table row here
+
+    private Long project_id;
+
+    private String name;
+
+    private Date from_date;
+
+    private Date to_date;
+
+    public Long getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(Long project_id) {
+        this.project_id = project_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getFrom_date() {
+        return from_date;
+    }
+
+    public void setFrom_date(Date from_date) {
+        this.from_date = from_date;
+    }
+
+    public Date getTo_date() {
+        return to_date;
+    }
+
+    public void setTo_date(Date to_date) {
+        this.to_date = to_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Project - " + name;
+    }
 }
