99f7d8ee761fcda74e30aecaf4fa8b6af0b5ece8
Jared Crist <cristjl1@gmail.com>
2021-06-20 22:54:19 -0400

    Department Test Setup

--- module-2/07_Database_Connectivity_DAO/exercise-student/java/src/test/java/com/techelevator/model/jdbc/JDBCDepartmentDAOIntegrationTest.java
+++ module-2/07_Database_Connectivity_DAO/exercise-student/java/src/test/java/com/techelevator/model/jdbc/JDBCDepartmentDAOIntegrationTest.java
@@ -1,4 +1,48 @@
 package com.techelevator.model.jdbc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.techelevator.projects.model.jdbc.JDBCDepartmentDAO;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class JDBCDepartmentDAOIntegrationTest {
+
+    private static final String TEST_DEPARTMENT = "ABC123 Department Test";
+
+    private static SingleConnectionDataSource dataSource;
+
+    private JDBCDepartmentDAO dao;
+
+    @BeforeClass
+    public static void setupDataSource() {
+        dataSource = new SingleConnectionDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/projectsDAO");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres1");
+        dataSource.setAutoCommit(false);
+    }
+
+    @AfterClass
+    public static void closeDataSource() throws SQLException {
+        dataSource.destroy();
+    }
+
+
+
 }
