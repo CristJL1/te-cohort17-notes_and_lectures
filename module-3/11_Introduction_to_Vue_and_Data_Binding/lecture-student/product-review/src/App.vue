22dad47610b16b73b29c562ecee3b182f5c5947f
Jared Crist <cristjl1@gmail.com>
2021-07-19 12:12:32 -0400

    Lecture Notes

---
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/lecture-student/product-review/src/App.vue
@@ -0,0 +1,43 @@
+<!-- App.vue is the homepage for a Vue application - it's displayed when the app is started -->
+<!-- the contents of the App.vue are inserted into the index.html page when the app is started -->
+<!-- main.js is the main JavaScript program for Vue and it casuses the App.vue to be inserted into the index -->
+<!-- this is where you add the necessary code for any Vue component to be used -->
+
+<template> <!-- HTML for the Vue component -->
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- a Vue component is coded as a tag in your HTML -->
+    <!-- HelloWorld is a Vue component in a file called HelloWorld.vue -->
+    <!-- Vue components are coded in kabob-case when used in the HTML -->
+    <!-- kabob-case is a name in all lowercase with dashes (-) between the main parts -->
+    <!-- using kabob-case makes it easier to distinguish vue components from normal HTML tags -->
+    <product-review/> <!-- using a Vue component called ProductReview.vue-->
+  </div>
+</template>
+
+<script> // JavaScript for the component
+// import the Vue component into the JavaScipt for the app
+// src folder is the root folder for the app
+// so all directory reference are relative to the src folder
+import ProductReview from './components/ProductReview.vue' // access the ProductReview component
+
+// allow Vue access to the data in the JavaScript code so it can use it and manipulate it
+// for a Vue component the JavaScript code holds all the data
+export default { // export exposes data in the JavaScript code to outside processes, like Vue
+  name: 'App', // name of the application - associate this data object with an application
+  components: { // list of Vue component used in the Application
+    ProductReview // name of a Vue component - must match the name on the import
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/lecture-student/product-review/src/components/ProductReview.vue
@@ -0,0 +1,80 @@
+<template> <!-- contains the HTML for our component --> 
+    <div class="main"> <!-- a Vue template must have a single div -->
+        <!-- use a value from data in the JavaScript code in the HTML -->
+        <!-- code name of the varaible or attribute in a mustache expression -->
+        <!-- a mustache expression is enclosed in {{}} -->
+        <!-- use of a mustache expression is referred to as one-way data binding -->
+        <!-- one-way data binding means data can flow from the JavaScript code to the HTML -->
+        <!-- two-way data binding means data can flow from the JavaScript code to the HTML and vice versa -->
+        <h2> Product Reviews for {{title}} </h2>
+        <p class='description'>  {{words}} </p>
+
+    </div>
+    
+
+</template>
+
+<script> // contains the JavaScript for the component
+export default { // expose the data in this JavaScript to Vue - allow Vue to acces this data
+    name: 'product-review', // name of the component in kabob-case (matches name in HTML tag)
+    data() {                // data to be shared with Vue needs to be in a function called data()
+        return {
+            title: 'Cigar Parties for Dummies', // variable to share with Vue
+            words: 'Host and plan the perfect cigar party for your squirelly friends'
+        } // end of return
+    } // end of data()
+} // end of export
+    
+
+</script>
+
+<style scoped>  /*contains the CSS for our component*/
+
+div.main {
+  margin: 1rem 0;
+}
+div.main div.well-display {
+  display: flex;
+  justify-content: space-around;
+}
+div.main div.well-display div.well {
+  display: inline-block;
+  width: 15%;
+  border: 1px black solid;
+  border-radius: 6px;
+  text-align: center;
+  margin: 0.25rem;
+}
+div.main div.well-display div.well span.amount {
+  color: darkslategray;
+  display: block;
+  font-size: 2.5rem;
+}
+div.main div.review {
+  border: 1px black solid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+}
+div.main div.review.favorited {
+  background-color: lightyellow;
+}
+div.main div.review div.rating {
+  height: 2rem;
+  display: inline-block;
+  vertical-align: top;
+  margin: 0 0.5rem;
+}
+div.main div.review div.rating img {
+  height: 100%;
+}
+div.main div.review p {
+  margin: 20px;
+}
+div.main div.review h3 {
+  display: inline-block;
+}
+div.main div.review h4 {
+  font-size: 1rem;
+}
+</style>
